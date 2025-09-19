c636af4f0ca873b292ce71f52e9066f795d29b8f
TlokotseSM <tlokotsemogudi@gmail.com>
2025-09-19 16:28:15 +0200

    tk's web portfolio

--- src/components/Certifications/Certifications.js
+++ src/components/Certifications/Certifications.js
@@ -9,77 +9,77 @@ function Certifications() {
     {
       title: "PHP From Scratch Beginner To Advanced",
       issuer: "Udemy",
-      pdfPath: "/certificates/PHP_From_Scratch.pdf"
+      pdfPath: "https://drive.google.com/file/d/1XGFhVOqAi7-QTFax19JkxFmcM40g-1zS/view?usp=sharing"
     },
     {
       title: "The Complete Java Certification Course",
       issuer: "Udemy",
-      pdfPath: "/certificates/Complete_Java_Certification.pdf"
+      pdfPath: "https://drive.google.com/file/d/1iwY7WausvXmaaPNneYysHbh-RwPcP2qM/view?usp=sharing"
     },
     {
       title: "One Week Python",
       issuer: "Udemy",
-      pdfPath: "/certificates/One_Week_Python.pdf"
+      pdfPath: "https://drive.google.com/file/d/1oN9dnR_uRZ1ZL245YIgAk6uWOw1jK--_/view?usp=sharing"
     },
     {
       title: "The HTML & CSS Bootcamp 2025 Edition",
       issuer: "Udemy",
-      pdfPath: "/certificates/HTML_CSS_Bootcamp_2025.pdf"
+      pdfPath: "https://drive.google.com/file/d/1OJP5xJdv_I8S8kfzhkliXmyvkEePpX-Q/view?usp=sharing"
     },
     {
       title: "SDLC - Software Development Life Cycle",
       issuer: "Udemy",
-      pdfPath: "/certificates/SDLC_Certificate.pdf"
+      pdfPath: "https://drive.google.com/file/d/1TcZ06E2WvUUeUJ85HuBz6YUVvEdNGQlE/view?usp=sharing"
     },
     {
       title: "The Complete 2025 Software Testing Bootcamp",
       issuer: "Udemy",
-      pdfPath: "/certificates/Software_Testing_Bootcamp_2025.pdf"
+      pdfPath: "https://drive.google.com/file/d/1zR-eSbRL9ZTjh4_aZQfmzOIkRwoHmDV7/view?usp=sharing"
     },
     {
       title: "Agile Retrospective+Continuous Improvement+Kaizen with Scrum",
       issuer: "Udemy",
-      pdfPath: "/certificates/Agile_Retrospective_Kaizen.pdf"
+      pdfPath: "https://drive.google.com/file/d/1hTwsU2DLM8kNOTpj-VJCgRRDyJSGhpfX/view?usp=sharing"
     },
     {
       title: "Business Analysis - Developing Requirements",
       issuer: "Udemy",
-      pdfPath: "/certificates/Business_Analysis_Requirements.pdf"
+      pdfPath: "https://drive.google.com/file/d/1sizY0TLqMkafB56tp9EJp7NpQkYGvAny/view?usp=sharing"
     },
     {
       title: "ChatGPT AI Ethics Ethical Intelligence for 2025",
       issuer: "Udemy",
-      pdfPath: "/certificates/ChatGPT_AI_Ethics.pdf"
+      pdfPath: "https://drive.google.com/file/d/1UrSkHiy-KAr7dZgiHfnzFAW-UM4H2Da9/view?usp=sharing"
     },
     {
       title: "Code Reviews for Secure, Clean, and Scalable Code",
       issuer: "Udemy",
-      pdfPath: "/certificates/Code_Reviews_Secure_Clean.pdf"
+      pdfPath: "https://drive.google.com/file/d/13ovPMS8ZEBH6An33SClnhSLl-5r7t7Iv/view?usp=sharing"
     },
     {
       title: "Continuous Improvement - Operational Excellence",
       issuer: "Udemy",
-      pdfPath: "/certificates/Continuous_Improvement_Excellence.pdf"
+      pdfPath: "https://drive.google.com/file/d/18lcW5zRqgp2zupTz8iKjGIe4uTjrPFRm/view?usp=sharing"
     },
     {
       title: "Fundamentals of Software Design and Architecture Course",
       issuer: "Udemy",
-      pdfPath: "/certificates/Software_Design_Architecture.pdf"
+      pdfPath: "https://drive.google.com/file/d/1QeMvvrByScFhx_qI6dZsOW7x6INt7ZId/view?usp=sharing"
     },
     {
       title: "Introduction to Design Thinking",
       issuer: "Udemy",
-      pdfPath: "/certificates/Introduction_Design_Thinking.pdf"
+      pdfPath: "https://drive.google.com/file/d/1t5ZEo_StFYeLbqH6uFW5G5jfwI1PeULQ/view?usp=sharing"
     },
     {
       title: "Understand and Elicit Requirements with Business Analysis",
       issuer: "Udemy",
-      pdfPath: "/certificates/Elicit_Requirements_Business_Analysis.pdf"
+      pdfPath: "https://drive.google.com/file/d/135nsg1CbsgbHubcy3o8c4P9PR7BycA_b/view?usp=sharing"
     },
     {
       title: "Git & GitHub Mastery: Zero to Hero",
       issuer: "Udemy",
-      pdfPath: "/certificates/Git_GitHub_Mastery.pdf"
+      pdfPath: "https://drive.google.com/file/d/1TacxhdSMex5lsgwkpMFBSKdpjf4rRcHh/view?usp=sharing"
     }
   ];
 
